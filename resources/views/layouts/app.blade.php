dff4440681287efd1a5b37b60f8887c7fb6353f3
Thilina Thushan <thilinathushan2U@gmail.com>
2025-02-08 19:22:23 +0530

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,12 +29,12 @@
     <meta name="twitter:image" content="{{ asset('love-thilinathushan-dev.png') }}">
 
 
-    <link rel="icon" type="image/png" href="{{ asset('/favicon/favicon-96x96.png') }}" sizes="96x96" />
-    <link rel="icon" type="image/svg+xml" href="{{ asset('/favicon/favicon.svg') }}" />
-    <link rel="shortcut icon" href="{{ asset('/favicon/favicon.ico') }}" />
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/favicon/apple-touch-icon.png') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}" />
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}" />
     <meta name="apple-mobile-web-app-title" content="Be My Valentine" />
-    <link rel="manifest" href="{{ asset('/favicon/site.webmanifest') }}" />
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}" />
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
